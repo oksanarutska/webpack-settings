3a4a53b10f75edb366aa911bf2a6271a7dd0f07c
Oksana Ruts <oksana@thefirstthelast.agency>
2020-05-17 17:50:00 +0300

    checked all project

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,9 @@ import { test } from '../../js/test';
 import $ from 'jquery';
 
 
+
+
 $(function() {
     $('body').css('color', 'blue');
+    $('body').css('background', `url(./images/main-back.jpg)`)
 });
